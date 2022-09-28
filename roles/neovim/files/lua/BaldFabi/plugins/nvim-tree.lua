2b1346743e24d438ed1ce55f54939dc1b9d3f5d2
BaldFabi <thebaldfabi@gmail.com>
2022-09-28 21:33:29 +0200

    Refactor(neovim): Change some mappings

--- roles/neovim/files/lua/BaldFabi/plugins/nvim-tree.lua
+++ roles/neovim/files/lua/BaldFabi/plugins/nvim-tree.lua
@@ -1,17 +1,28 @@
 local function init()
+    local map = vim.api.nvim_set_keymap
+    local options = { noremap = false }
+
     require("nvim-tree").setup({
         sort_by = "case_sensitive",
         --open_on_setup = true,
         --open_on_setup_file = true,
         --open_on_tab = true,
         view = {
-            adaptive_size = false
+            adaptive_size = false,
+            mappings = {
+                list = {
+                    { key = "+", action = "vsplit" },
+                    { key = "-", action = "split" },
+                }
+            }
         },
         update_focused_file = {
             enable = true,
             update_cwd = true,
-        },
+        }
     })
+
+    map('n', '<leader>t', '<CMD>NvimTreeToggle<CR>', options)
 end
 
 return {
