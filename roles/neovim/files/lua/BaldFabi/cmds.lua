9f2fde3a97e0c6575a0e920d0132aad5d242ebde
BaldFabi <thebaldfabi@gmail.com>
2022-10-03 19:55:09 +0200

    Feat(neovim): Add import organization for go

--- roles/neovim/files/lua/BaldFabi/cmds.lua
+++ roles/neovim/files/lua/BaldFabi/cmds.lua
@@ -1,6 +1,6 @@
 local function init()
     vim.opt.list = true
-    vim.opt.listchars:append "eol:↴"
+    --vim.opt.listchars:append "eol:↴"
     --vim.opt.listchars:append "space:⋅"
     vim.opt.tabstop = 4
     vim.opt.shiftwidth = 4
@@ -18,10 +18,12 @@ local function init()
     vim.opt.termguicolors = true
     vim.opt.wildmenu = true
     vim.opt.cursorline = true
-    vim.cmd('set number relativenumber')
+    vim.opt.number = true
+    vim.opt.relativenumber = true
 
-    --    vim.api.nvim_command('autocmd VimResized * wincmd =')
+    vim.api.nvim_command('autocmd VimResized * wincmd =')
     vim.api.nvim_command('autocmd BufWritePre *.js,*.tsx,*.ts,*.md,*.css,*.scss,*.sass,*.yaml,*.yml,*.json,*.html :Neoformat')
+    vim.api.nvim_command("autocmd BufWritePre *.go :lua require 'BaldFabi.plugins.lspconfig'.goimports(1000)")
 
     vim.cmd [[packadd packer.nvim]]
     vim.cmd [[colorscheme onedark]]
--- roles/neovim/files/lua/BaldFabi/plugins/lspconfig.lua
+++ roles/neovim/files/lua/BaldFabi/plugins/lspconfig.lua
@@ -1,3 +1,22 @@
+local function goimports(timeout_ms)
+    timeout_ms = timeout_ms or 1000
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+    for _, res in pairs(result) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit and not vim.tbl_isempty(r.edit) then
+                vim.lsp.util.apply_workspace_edit(r.edit, 'UTF-8')
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+
+    vim.lsp.buf.formatting_sync(nil, timeout_ms)
+end
+
 local function init()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -7,11 +26,14 @@ local function init()
     end
 
     require 'lspconfig'.gopls.setup {
-        on_attach = on_attach
+        capabilities = capabilities,
+        on_attach = on_attach,
     }
     require 'lspconfig'.tsserver.setup {
+        capabilities = capabilities,
     }
     require 'lspconfig'.pylsp.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.html.setup {
@@ -24,34 +46,43 @@ local function init()
         capabilities = capabilities,
     }
     require 'lspconfig'.sqlls.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.tailwindcss.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.yamlls.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.bashls.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.dockerls.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.eslint.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.ansiblels.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.marksman.setup {
+        capabilities = capabilities,
         on_attach = on_attach
     }
     require 'lspconfig'.terraform_lsp.setup {
+        capabilities = capabilities,
         on_attach = on_attach
-        --
     }
     require 'lspconfig'.sumneko_lua.setup {
+        capabilities = capabilities,
         settings = {
             Lua = {
                 runtime = {
@@ -73,5 +104,6 @@ local function init()
 end
 
 return {
-    init = init
+    init = init,
+    goimports = goimports
 }
