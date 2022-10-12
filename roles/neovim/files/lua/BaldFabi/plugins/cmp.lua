005d8d54b0adcab6ff3f0f5b4cd191a18b8c837a
BaldFabi <thebaldfabi@gmail.com>
2022-10-12 19:15:21 +0200

    Refactor(neovim): Deactivate tab shortcuts for cmp

--- roles/neovim/files/lua/BaldFabi/plugins/cmp.lua
+++ roles/neovim/files/lua/BaldFabi/plugins/cmp.lua
@@ -9,20 +9,20 @@ local function init()
             end,
         },
         mapping = cmp.mapping.preset.insert({
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
+            --['<Tab>'] = cmp.mapping(function(fallback)
+            --if cmp.visible() then
+            --cmp.select_next_item()
+            --else
+            --fallback()
+            --end
+            --end, { "i", "s" }),
+            --['<S-Tab>'] = cmp.mapping(function(fallback)
+            --if cmp.visible() then
+            --cmp.select_prev_item()
+            --else
+            --fallback()
+            --end
+            --end, { "i", "s" }),
             ['<CR>'] = cmp.mapping.confirm({ select = true }),
             ['<C-Space>'] = cmp.mapping.complete()
         }),
