7f2d9294b1656df52f9b9d86aa8175f98ecbdfd4
BaldFabi <thebaldfabi@gmail.com>
2023-02-05 16:43:03 +0100

    Feat(nvim): Add illuminate

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -88,6 +88,15 @@ require('packer').startup(function(use)
     end
   }
 
+  use {
+    'RRethy/vim-illuminate',
+    config = function()
+      require 'illuminate'.configure({
+        delay = 0
+      })
+    end
+  }
+
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
