61ec8b71766768081efaabb431a4a95932816527
BaldFabi <thebaldfabi@gmail.com>
2023-08-31 21:49:57 +0200

    Refactor(nvim): Change some settings for nvimtree

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -189,12 +189,6 @@ require('packer').startup(function(use)
         respect_buf_cwd = true,
         update_focused_file = {
           enable = true,
-          update_cwd = true,
-        },
-        actions = {
-          open_file = {
-            quit_on_open = true
-          }
         },
         view = {
           side = "left",
